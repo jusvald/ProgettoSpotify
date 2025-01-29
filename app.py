cb454a8924d7557ee3aeb29ff3a53433575b45c8
jusvald vald <valderama.justin@iisgalvanimi.edu.it>
2025-01-29 09:57:47 +0000

    PrimaLezione

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, redirect, request, url_for, render_template,session
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+#le tue credenziali le trovi nella dashboard di prima
+SPOTIFY_CLIENT_ID = "9ea7ac3f15ab48a1ad58696bb21d49b6"
+SPOTIFY_CLIENT_SECRET = "acb51c389e85423e9306879c66d9cb75"
+SPOTIFY_REDIRECT_URI = "https://5000-jusvald-progettospotify-991nvdhsm4u.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+
+app = Flask(__name__)
+app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
+
+#config SpotifyOAuth per l'autenticazione e redirect uri
+sp_oauth = SpotifyOAuth(
+client_id=SPOTIFY_CLIENT_ID,
+client_secret=SPOTIFY_CLIENT_SECRET,
+redirect_uri=SPOTIFY_REDIRECT_URI,
+scope="user-read-private" #permessi x informazioni dell'utente
+)
+
+@app.route('/')
+def login():
+    auth_url = sp_oauth.get_authorize_url() #login di spotify
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code') #recupero codice di autorizzazione
+    token_info = sp_oauth.get_access_token(code) #uso il code per un codice di accesso
+    session['token_info'] = token_info #salvo il token nella mia sessione x riutilizzarlo
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    token_info = session.get('token_info', None) #recupero token sessione (salvato prima)
+    if not token_info:
+        return redirect(url_for('login'))
+    sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
+    user_info = sp.current_user()
+    print(user_info) #capiamo la struttura di user_info per usarle nel frontend
+    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
+
+if __name__ == '__main__': 
+    app.run(debug=True)
